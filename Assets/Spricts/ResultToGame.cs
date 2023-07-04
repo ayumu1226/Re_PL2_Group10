c333a09826878df82a64a526863b231318421b41
ayumu1226 <ayumu.f.1226@gmail.com>
2023-07-04 15:04:09 +0900

    Change ResultScene

---
+++ Assets/Spricts/ResultToGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultToGame : MonoBehaviour
+{
+    public void Resultbutton1()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
