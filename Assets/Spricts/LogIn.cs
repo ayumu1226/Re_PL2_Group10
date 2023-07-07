120ecf036fecc931a109192125e9102101bcfc3b
Riku Honda <hondariku@gmail.com>
2023-07-07 17:29:20 +0900

    loginスクリプト修正

--- Assets/Spricts/LogIn.cs
+++ Assets/Spricts/LogIn.cs
@@ -5,36 +5,26 @@ using NCMB;
 using UnityEngine.SceneManagement;
 
 public class LogIn : MonoBehaviour
-{  
-    
+{
     public InputField UserName;
     public InputField PassWord;
     private string currentPlayerName;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-  
-
-    public void Login()
+    public void LogInButton()
     {
         print(UserName.text);
         print(PassWord.text);
 
-        //★NCMBUserのインスタンス作成 
-        NCMBUser user = new NCMBUser();
-
-        // ★ユーザー名とパスワードでログイン
         NCMBUser.LogInAsync(UserName.text, PassWord.text, (NCMBException e) => {
             if (e != null)
             {
@@ -43,50 +33,44 @@ public class LogIn : MonoBehaviour
             else
             {
                 UnityEngine.Debug.Log("ログインに成功！");
-                //画面遷移
                 SceneManager.LoadScene("ChooseLevelScene");
             }
         });
     }
-    public void SignUp()
+
+    public void SignUpButton()
     {
         print(UserName.text);
         print(PassWord.text);
 
-        //NCMBUserのインスタンス作成 
         NCMBUser user = new NCMBUser();
-
-        //ユーザ名とパスワードの設定
         user.UserName = UserName.text;
         user.Password = PassWord.text;
 
-        //会員登録
         user.SignUpAsync((NCMBException e) => {
             if (e != null)
             {
                 UnityEngine.Debug.Log("新規登録に失敗: " + e.ErrorMessage);
-                SceneManager.LoadScene(*); 
+                SceneManager.LoadScene("遷移先のシーン名");
             }
             else
             {
                 UnityEngine.Debug.Log("新規登録に成功");
-                //★カレントユーザーを確認
                 NCMBUser currentUser = NCMBUser.CurrentUser;
                 if (currentUser != null)
                 {
                     UnityEngine.Debug.Log("ログイン中のユーザー: " + currentUser.UserName);
-                    Login();
+                    LogInButton();
                 }
                 else
                 {
                     UnityEngine.Debug.Log("未ログインまたは取得に失敗");
                 }
-                //画面遷移
-                SceneManager.LoadScene(*);//遷移させたいシーンを指定
+                SceneManager.LoadScene("遷移先のシーン名");
             }
         });
     }
-    
+
     public void Userlogout()
     {
         NCMBUser.LogOutAsync((NCMBException e) => {
@@ -100,5 +84,4 @@ public class LogIn : MonoBehaviour
             }
         });
     }
-
 }
