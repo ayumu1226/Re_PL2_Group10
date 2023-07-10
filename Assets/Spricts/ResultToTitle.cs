2ed617884a44a6e5b2860b67502d8b4c49406773
ayumu.f <ayumu.f.1226@gmail.com>
2023-07-11 02:12:04 +0900

    ボタンアニメーション追加

--- Assets/Spricts/ResultToTitle.cs
+++ Assets/Spricts/ResultToTitle.cs
@@ -32,6 +32,6 @@ public class ResultToTitle : MonoBehaviour
 
     public void Resultbutton2()
     {
-        SceneManager.LoadScene("TitleScene");
+        SceneManager.LoadScene("ChooseModeScene");
     }
 }
