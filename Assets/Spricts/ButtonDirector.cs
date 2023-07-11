9aa0df90154ea8dc09d9e756eaa59f635a61cf2f
ayumu1226 <ayumu.f.1226@gmail.com>
2023-07-11 14:44:27 +0900

    難易度選択

--- Assets/Spricts/ButtonDirector.cs
+++ Assets/Spricts/ButtonDirector.cs
@@ -5,34 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class ButtonDirector : MonoBehaviour
 {
+    public static int mode;
+    public static int level;
+
     private void Start()
     {
-        
+        mode = 0;
+        level = 0;
     }
 
     public void button1() // 通常モード
     {
+        mode = 1;
         SceneManager.LoadScene("ChooseLevelScene");
     }
     public void button2() // 慎重モード
     {
+        mode = 2;
         SceneManager.LoadScene("ChooseLevelScene");
     }
     public void button3() // 練習モード
     {
+        mode = 3;
         SceneManager.LoadScene("ChooseLevelScene");
     }
 
     public void button4() // easy
     {
+        level = 1;
         SceneManager.LoadScene("GameScene");
     }
     public void button5() // normal
     {
+        level = 2;
         SceneManager.LoadScene("GameScene");
     }
     public void button6() // hard
     {
+        level = 3;
         SceneManager.LoadScene("GameScene");
+    }
+
+    public static int GetMode()
+    {
+        return mode;
+    }
+
+    public static int GetLevel()
+    {
+        return level;
     }
 }
