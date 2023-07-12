3ea9f55364766a3d4fc489a5d2c1a1d9dd0a8716
ayumu.f <ayumu.f.1226@gmail.com>
2023-07-12 23:28:26 +0900

    ChooseLeaderBoard変更

---
+++ Assets/Spricts/RankingManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RankingManager : MonoBehaviour
+{
+    public static int flag;
+
+    public void Mode1Button()
+    {
+        flag = 1;
+        SceneManager.LoadScene("LeaderBoard");
+    }
+
+    public void Mode2Button()
+    {
+        flag = 2;
+        SceneManager.LoadScene("LeaderBoard");
+    }
+
+    public static int GetFlag()
+    {
+        return flag;
+    }
+}
